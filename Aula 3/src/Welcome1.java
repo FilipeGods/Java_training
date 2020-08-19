9513c806d2e7e57fe25070a06c986f5880bc0e7f
FilipeGods <54822974+FilipeGods@users.noreply.github.com>
2020-08-19 08:06:07 -0300

    Add files via upload

---
+++ Aula 3/src/Welcome1.java
@@ -0,0 +1,13 @@
+// Welcome1.java
+// O programa imprime uma frase.
+
+public class Welcome1
+{
+   // método main inicia a execução da aplicação Java
+   public static void main( String[] args )
+   {
+      System.out.println( "Welcome to Java Programming!" );
+   } // fim do método main
+} // fim da classe Welcome1
+
+
